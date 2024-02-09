ca9be53865a389bd5d2e8994e319ea571a52f8fa
diana2x <dianaclxcoe@gmail.com>
2024-02-09 02:33:48 -0600

    created login and userdashboard skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import LoginForm from './Components/LoginForm/LoginForm';
+import Dashboard from './Components/Dashboard/Dashboard';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <div><LoginForm /></div>
+  },
+  {
+    path: '/dashboard',
+    element: <div><Dashboard /></div>
+  },
+])
 
 function App() {
   return (
     <div>
+      <RouterProvider router={(router)} />
     </div>
   );
-}
+};
 
 export default App;
+
